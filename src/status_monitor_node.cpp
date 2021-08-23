2f6aaa622371c341b272f042a9b8edf5bbd02cb5
H01 <marblerobots@gmail.com>
2021-08-23 12:41:13 -0600

    Replaced ROS_INFO with printf. Outstanding issues: alphabetical order, labels

--- src/status_monitor_node.cpp
+++ src/status_monitor_node.cpp
@@ -9,6 +9,7 @@
 #include <iomanip>
 
 using namespace RosIntrospection;
+using namespace std;
 using topic_tools::ShapeShifter;
 
 struct topicInfo
@@ -16,17 +17,11 @@ struct topicInfo
     double freq_expected;
     double tolerance;
     bool initialized = false;
-    bool is_publishing = false;
 
-    ros::Time last_time;
-    ros::Time curr_time;
-    double last_freq = 0.0;
-    double freq = 0.0;
-    double curr_freq = 0.0;
-    int horizon = 100;
-    int counter = 0;
-    // std::vector<double> pub_times = std::vector<double>(horizon);
-    std::vector<double> pub_times; // has to be a better way to do this
+    double freq;
+    int counter;
+    // vector<double> pub_times = vector<double>(horizon);
+    vector<double> pub_times; // has to be a better way to do this
     
     topicInfo(double freq_ex, double tol)
     : freq_expected(freq_ex),
@@ -34,52 +29,65 @@ struct topicInfo
     {};
 };
 
-int init;
-std::string vehicle_name;
+bool init;
+string vehicle_name;
+float print_rate;
 XmlRpc::XmlRpcValue topic_list;
-std::map<std::string, topicInfo> topics;
+map<string, topicInfo> topics;
 
-void topicCallback(const ShapeShifter::ConstPtr& msg, const std::string& topic_name,
+void topicCallback(const ShapeShifter::ConstPtr& msg, const string& topic_name,
                    RosIntrospection::Parser& parser)
 {
-    std::map<std::string, topicInfo>::iterator it = topics.find(topic_name);
+    map<string, topicInfo>::iterator it = topics.find(topic_name);
     auto info = &(it->second);
     info->counter++;
 }
 
 void monitorCallback(const ros::TimerEvent& event)
-{   
-    if(init < 10)
-    {
-        init++;
-    ROS_INFO("\033\143"); // This clears the terminal so it appears like it's updating instead of printing continuously
+{
+    if(!init) {
+        printf("\033\143"); // This clears the terminal so it appears like it's updating instead of printing continuously
+        int num_topics = topics.size();
+        string color;
+        for (auto it = topics.begin(); it != topics.end(); ++it) {
+            topicInfo* topic = &(it->second);
+            topic->freq = topic->counter * print_rate;
+            if(isnan(topic->freq)) {
+                topic->freq = 0.0;
+            }
+            else if((topic->freq > topic->freq_expected + topic->tolerance) || 
+                (topic->freq < topic->freq_expected - topic->tolerance)) {
+            }
+            else {
+            }
+            topic->counter = 0;
+        }
+        init = 1;
     }
-    else{
-        
-    ROS_INFO("\033\143"); // This clears the terminal so it appears like it's updating instead of printing continuously
-    int num_topics = topics.size();
-    std::string color;
-    for (auto it = topics.begin(); it != topics.end(); ++it) {
-        topicInfo* topic = &(it->second);
-        topic->freq = topic->counter;
-        if(isnan(topic->freq)) {
-            topic->freq = 0.0;
-            ROS_INFO("\033[1;31m NO MSG\t%.1f\t%s\033[0m", topic->freq, it->first.c_str());
+    else {
+        printf("\033\143"); // This clears the terminal so it appears like it's updating instead of printing continuously
+        int num_topics = topics.size();
+        string color;
+        for (auto it = topics.begin(); it != topics.end(); ++it) {
+            topicInfo* topic = &(it->second);
+            topic->freq = topic->counter * print_rate;
+            if(isnan(topic->freq)) {
+                topic->freq = 0.0;
+                printf("\033[1;31m NO MSG\t%.1f\t%s\033[0m \n", topic->freq, it->first.c_str());
+            }
+            else if((topic->freq > topic->freq_expected + topic->tolerance) || 
+                (topic->freq < topic->freq_expected - topic->tolerance)) {
+                printf("\033[1;31m FAIL\t%.1f\t%s\033[0m \n", topic->freq, it->first.c_str());
+            }
+            else {
+                printf("\033[1;32m PASS\t%.1f\t%s\033[0m \n", topic->freq, it->first.c_str());
+            }
+            topic->counter = 0;
         }
-        else if((topic->freq > topic->freq_expected + topic->tolerance) || 
-            (topic->freq < topic->freq_expected - topic->tolerance)) {
-            ROS_INFO("\033[1;31m FAIL\t%.1f\t%s\033[0m", topic->freq, it->first.c_str());
-            topic->is_publishing = true;
-        }
-        else {
-            ROS_INFO("\033[1;32m PASS\t%.1f\t%s\033[0m", topic->freq, it->first.c_str());
-        }
-        topic->counter = 0;
-    }
     }
 }
 
-void loadFromConfig(ros::NodeHandle& nh, std::vector<ros::Subscriber>& subs)
+void loadFromConfig(ros::NodeHandle& nh, vector<ros::Subscriber>& subs)
 {
     for (int i = 0; i < topic_list.size(); i++) {
         XmlRpc::XmlRpcValue monitor_iter = topic_list[i];
@@ -87,7 +95,7 @@ void loadFromConfig(ros::NodeHandle& nh, std::vector<ros::Subscriber>& subs)
 
         // verify topic name is type string 
         ROS_ASSERT(monitor_iter["topic"].getType() == XmlRpc::XmlRpcValue::TypeString);
-        std::string topic_name = monitor_iter["topic"];
+        string topic_name = monitor_iter["topic"];
 		topic_name = "/" + vehicle_name + "/" + topic_name;
 
         // verify expected publishing rate is type double
@@ -100,7 +108,7 @@ void loadFromConfig(ros::NodeHandle& nh, std::vector<ros::Subscriber>& subs)
 
         // create new struct to hold topic information
         topicInfo newTopic(expected_hz, tolerance);
-        topics.insert(std::pair<std::string, topicInfo> (topic_name, newTopic));
+        topics.insert(pair<string, topicInfo> (topic_name, newTopic));
         
         // create callback for topic and add to subscriber list
         Parser parser;
@@ -118,9 +126,10 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "status_monitor");
     ros::NodeHandle nh;
+    nh.getParam("print_rate", print_rate);
     nh.getParam("vehicle_name", vehicle_name);
     nh.getParam("topic_list", topic_list);
-    std::vector<ros::Subscriber> topic_subs;
+    vector<ros::Subscriber> topic_subs;
     loadFromConfig(nh, topic_subs);
 
     // for debugging
@@ -129,10 +138,10 @@ int main(int argc, char** argv)
     // for (const auto& topic_info: advertized_topics) {
     //     if ((topic_info.name == "/rosout") || (topic_info.name == "/rosout_agg"))
     //         continue;
-    //     ROS_INFO("\033[1;35m--> Advertised: %s \033[0m", topic_info.name.c_str());
+    //     cout << "\033[1;35m--> Advertised: %s \033[0m", topic_info.name.c_str());
     // }
     
-    ros::Timer monTimer = nh.createTimer(ros::Duration(1), monitorCallback);
+    ros::Timer monTimer = nh.createTimer(ros::Duration(1 / print_rate), monitorCallback);
     ros::spin();
 	
     return 0;
